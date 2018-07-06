8763393d054cdf76b4ba9004613434c6cacd200a
Jon Fairbanks <fairplay89@gmail.com>
2018-07-05 20:58:45 -0700

    An Attempt at Adding Links to Links

--- src/variables/LinksConfig.jsx
+++ src/variables/LinksConfig.jsx
@@ -7,7 +7,7 @@ const tableTitle = "Helpful Tools";
 const tableSubtitle = "Helpful sites to have on hand when developing or deploying software.";
 const thArray = ["Name", "Type", "Description"];
 const tdArray = [
-  ["<a href='https://google.com' target='_blank'>Google</a>", "Murica", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas vestibulum dolor, vitae hendrerit erat volutpat in. Nulla facilisi. Vestibulum malesuada molestie magna, sit amet iaculis turpis viverra eu. Praesent."],
+  ["<NavLink to='https://google.com'>Google</NavLink>", "Murica", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas vestibulum dolor, vitae hendrerit erat volutpat in. Nulla facilisi. Vestibulum malesuada molestie magna, sit amet iaculis turpis viverra eu. Praesent."],
   ["Minerva Hooper", "Curaçao", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id orci hendrerit, porttitor purus vitae, mollis est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet ante sit amet."],
   ["Sage Rodriguez", "Netherlands", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar, velit id dapibus vestibulum, diam tellus lobortis elit, in porta tellus ex id nisl. Phasellus ipsum metus, tristique ac neque et, dapibus sollicitudin leo. Duis eget tempor magna. Nullam tristique egestas ligula."],
   ["Philip Chaney", "Korea, South", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc velit orci, blandit a porta vitae, sagittis vitae mi. Donec pellentesque elit mauris, eget lobortis dolor elementum sit amet. Praesent dictum."],
