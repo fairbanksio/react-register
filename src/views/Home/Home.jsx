357170f3ffc561d2ef4175096c267bfe1bdc40b4
Jon Fairbanks <fairplay89@gmail.com>
2018-07-06 01:00:01 -0700

    Pull the Number of Github Repos from the Github API

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,19 +4,14 @@ import { Grid, Row, Col } from "react-bootstrap";
 
 import { Card } from "components/Card/Card.jsx";
 import { StatsCard } from "components/StatsCard/StatsCard.jsx";
-import { Tasks } from "components/Tasks/Tasks.jsx";
 import {
-  dataPie,
-  legendPie,
   dataSales,
   optionsSales,
   responsiveSales,
-  legendSales,
-  dataBar,
-  optionsBar,
-  responsiveBar,
-  legendBar
+  legendSales
 } from "variables/Variables.jsx";
+
+import { userConfig } from "variables/UserConfig.jsx";
 
 class Home extends Component {
   createLegend(json) {
@@ -28,6 +23,27 @@ class Home extends Component {
       legend.push(json["names"][i]);
     }
     return legend;
+  }
+  getRepoCount() {
+    var _this = this;
+    var endpoint = 'https://api.github.com/users/' + userConfig.GitHub;
+    fetch(endpoint)
+    .then((resp) => resp.json())
+    .then(function(data){
+      console.log(data.public_repos);
+      _this.setState({"repos": data.public_repos});
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  }
+  componentWillMount() {
+    this.setState({
+      "repos": 0
+    });
+  }
+  componentDidMount() {
+    this.getRepoCount();
   }
   render() {
     return (
@@ -63,9 +79,9 @@ class Home extends Component {
             </Col>
             <Col lg={3} sm={6}>
               <StatsCard
-                bigIcon={<i className="fa fa-twitter text-info" />}
-                statsText="Followers"
-                statsValue="+45"
+                bigIcon={<i className="fa fa-github" />}
+                statsText="Repositories"
+                statsValue={this.state.repos}
                 statsIcon={<i className="fa fa-refresh" />}
                 statsIconText="Updated now"
               />
@@ -96,60 +112,22 @@ class Home extends Component {
             </Col>
             <Col md={4}>
               <Card
-                statsIcon="fa fa-clock-o"
-                title="Email Statistics"
-                category="Last Campaign Performance"
-                stats="Campaign sent 2 days ago"
+                statsIcon="fab fa-digital-ocean"
+                title={userConfig.SiteTitle + " Behind The Scenes"}
+                category="This site is brought to you by..."
+                stats="Powered by DigitalOcean"
                 content={
-                  <div
-                    id="chartPreferences"
-                    className="ct-chart ct-perfect-fourth"
-                  >
-                    <ChartistGraph data={dataPie} type="Pie" />
-                  </div>
-                }
-                legend={
-                  <div className="legend">{this.createLegend(legendPie)}</div>
-                }
-              />
-            </Col>
-          </Row>
-
-          <Row>
-            <Col md={6}>
-              <Card
-                id="chartActivity"
-                title="2014 Sales"
-                category="All products including Taxes"
-                stats="Data information certified"
-                statsIcon="fa fa-check"
-                content={
-                  <div className="ct-chart">
-                    <ChartistGraph
-                      data={dataBar}
-                      type="Bar"
-                      options={optionsBar}
-                      responsiveOptions={responsiveBar}
-                    />
-                  </div>
-                }
-                legend={
-                  <div className="legend">{this.createLegend(legendBar)}</div>
-                }
-              />
-            </Col>
-
-            <Col md={6}>
-              <Card
-                title="Tasks"
-                category="Backend development"
-                stats="Updated 3 minutes ago"
-                statsIcon="fa fa-history"
-                content={
-                  <div className="table-full-width">
-                    <table className="table">
-                      <Tasks />
-                    </table>
+                  <div>
+                    <ul>
+                      {/* TO DO: Load all of this from a config? */}
+                      <li><a href='https://github.com/Fairbanks-io/react-register' target='_blank' rel='noopener noreferrer'>This React Dashboard</a> with special thanks to Tim, Brandon and Jon</li>
+                      <li>Node, courtesy of <a href='https://github.com/creationix/nvm' target='_blank' rel='noopener noreferrer'>NVM</a></li>
+                      <li><a href='https://docs.docker.com/' target='_blank' rel='noopener noreferrer'>Docker Virtualization</a></li>
+                      <li>Load balancing, rate limiting and proxy support from <a href='https://nginx.org/en/' target='_blank' rel='noopener noreferrer'>Nginx!</a></li>
+                      <li>Modules such as <a href='https://expressjs.com/' target='_blank' rel='noopener noreferrer'>Express</a> and <a href='https://github.com/krakenjs/lusca' target='_blank' rel='noopener noreferrer'>Lusca</a></li>
+                      <li><a href='https://letsencrypt.org/' target='_blank' rel='noopener noreferrer'>Let's Encrypt</a> SSL certificates auto-renewed with <a href='https://github.com/srvrco/getssl' target='_blank' rel='noopener noreferrer'>GetSSL</a></li>
+                      <li><a href='https://www.splunk.com/en_us/software/splunk-enterprise.html' target='_blank' rel='noopener noreferrer'>Splunk</a> Monitoring and Alerting</li>
+                    </ul>
                   </div>
                 }
               />
