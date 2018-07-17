106aa0ac84fa4ad103ab5922f6bfd102357a43dc
Jon Fairbanks <fairplay89@gmail.com>
2018-07-16 23:14:07 -0700

    Fixed Skills Card with Color Support

--- src/components/SkillsCard/SkillsCard.jsx
+++ src/components/SkillsCard/SkillsCard.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import SkillBar from 'react-skillbars';
-import { skills } from 'variables/PortfolioConfig.jsx'
+import { skillsData, skillsColors } from 'variables/PortfolioConfig.jsx'
 
 export class SkillsCard extends Component {
   render() {
     return (
-      <SkillBar skills={skills} />
+      <SkillBar skills={skillsData} colors={skillsColors} />
     );
   }
 }
