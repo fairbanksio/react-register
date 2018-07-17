6c3ae8ab8734651f752d28e026414ba0460d3266
Jon Fairbanks <fairplay89@gmail.com>
2018-07-16 22:47:12 -0700

    Fixed SkillsCard

--- src/components/SkillsCard/SkillsCard.jsx
+++ src/components/SkillsCard/SkillsCard.jsx
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
 import SkillBar from 'react-skillbars';
-import skills from 'variables/UserConfig.jsx'
-
-var SKILLS = [
-  {type: "Node", level: 85},
-  {type: "Javascript", level: 75},
-];
+import { skills } from 'variables/PortfolioConfig.jsx'
 
 export class SkillsCard extends Component {
   render() {
     return (
-      <SkillBar skills={SKILLS} />
+      <SkillBar skills={skills} />
     );
   }
 }
