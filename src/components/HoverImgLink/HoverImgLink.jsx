43934d4403bf673aed108f5b8157c40144f55229
bschumacher <34971961+bschumacher@users.noreply.github.com>
2018-07-21 22:56:58 -0500

    new

--- src/components/HoverImgLink/HoverImgLink.jsx
+++ src/components/HoverImgLink/HoverImgLink.jsx
@@ -5,7 +5,7 @@ import './HoverImgLink.css';
 class HoverImgLink extends Component {
   render() {
     return (
-        <Col lg={3} md={4} sm={6} xs={12}>
+        <Col lg={3.5} md={4} sm={6} xs={12}>
           <div className="card">
             <a href={this.props.linkURL} target='_blank' rel='noopener noreferrer'>
               <div className="hover-container">
