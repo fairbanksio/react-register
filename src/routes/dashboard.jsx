aaf1283ec672fccdf14042bcdf42942498e61b30
Jon Fairbanks <fairplay89@gmail.com>
2018-07-27 18:21:19 -0700

    SEO Fix for Site Title

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import DynamicPage from "components/DynamicPage/DynamicPage";
 import siteData from "variables/SiteData.jsx";
 
-//dynamically make the routes from the config file
+// Dynamically make routes from the config file
 var sitePages = siteData.pages.map(function(page){
   if (page.redirect === true) {
     return { redirect: true, path: page.path, to: page.to, name: page.name}
   } else {
-    var pageComponent = (props) => <DynamicPage page={page}/>
+    var pageComponent = (props) => <DynamicPage page={page} website={siteData.Website} siteTitle={siteData.SiteTitle} />
     return { path: page.path, name: page.name, icon: page.icon, component: pageComponent, hidden: page.hidden}
   }
 })
