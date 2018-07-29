dc78e344d6f9c5bce789ed5cde8ad80273aa0502
bsord <brandon.sorgdrager@gmail.com>
2018-07-29 18:21:02 -0500

    Allow relative path definition in homepage.

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
 
 ReactDOM.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Switch>
       {indexRoutes.map((prop, key) => {
         return <Route to={prop.path} component={prop.component} key={key} />;
