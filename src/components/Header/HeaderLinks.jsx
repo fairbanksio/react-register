d0619898913228570c47c99120d9db8273044e63
Jon Fairbanks <fairplay89@gmail.com>
2018-11-17 20:58:40 -0800

    Remove Right Hand Header Link Defaults

--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -17,26 +17,7 @@ class HeaderLinks extends Component {
           {/* Additional LeftNav items can be added here. */}
         </Nav>
         <Nav pullRight>
-          <NavItem eventKey={1} href="#">
-            <i className="fa fa-dashboard" />
-            <p className="hidden-lg hidden-md">Dashboard</p>
-          </NavItem>
-          <NavDropdown
-            eventKey={2}
-            title={notification}
-            noCaret
-            id="basic-nav-dropdown"
-          >
-            <MenuItem eventKey={2.1}>Never</MenuItem>
-            <MenuItem eventKey={2.2}>Gonna</MenuItem>
-            <MenuItem eventKey={2.3}>Give</MenuItem>
-            <MenuItem eventKey={2.4}>You</MenuItem>
-            <MenuItem eventKey={2.5}>Up</MenuItem>
-          </NavDropdown>
-          <NavItem eventKey={3} href="#">
-            <i className="fa fa-search" />
-            <p className="hidden-lg hidden-md">Search</p>
-          </NavItem>
+          {/* Additional RightNav items can be added here. */}
         </Nav>
       </div>
     );
